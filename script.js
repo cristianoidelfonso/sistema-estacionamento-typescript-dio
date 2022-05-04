5da0765fe45abe120c2d7e8555435904bd4ba442
cristianoidelfonso <cristiano_idelfonso@hotmail.com>
2022-05-04 09:59:16 -0300

    Salvando registro no localStorage

--- script.js
+++ script.js
@@ -2,6 +2,31 @@
 (function () {
     var _a;
     const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() {
+            return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+        }
+        function salvar(veiculos) {
+            localStorage.setItem("patio", JSON.stringify(veiculos));
+        }
+        function adicionar(veiculo) {
+            var _a;
+            const row = document.createElement('tr');
+            row.innerHTML = `
+        <td>${veiculo.nome}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td><button class="delete" data-placa="${veiculo.placa}">X</button></td>
+      `;
+            (_a = $("#patio")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            salvar([...ler(), veiculo]);
+        }
+        function remover() {
+        }
+        function render() {
+        }
+        return { ler, adicionar, salvar, remover, render };
+    }
     (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
@@ -11,5 +36,6 @@
             alert("Os campos são obrigatorios");
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date() });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,6 +1,49 @@
+interface Veiculo {
+  nome: string,
+  placa: string,
+  entrada: Date,
+}
+
 (function() {
 
   const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+  function patio(){
+    
+    function ler(){
+      return localStorage.patio ? JSON.parse(localStorage.patio) : [];
+    }
+
+    function salvar(veiculos: Veiculo[]){
+      localStorage.setItem("patio", JSON.stringify(veiculos));
+    }
+
+    function adicionar(veiculo: Veiculo){
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+        <td>${veiculo.nome}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td><button class="delete" data-placa="${veiculo.placa}">X</button></td>
+      `;
+
+      $("#patio")?.appendChild(row);
+
+      salvar([...ler(), veiculo]);
+
+    }
+    
+    function remover(){
+
+    }
+
+    function render(){
+
+    }
+
+    return { ler, adicionar, salvar, remover, render };
+  }
 
   $("#cadastrar")?.addEventListener("click", () => {
     const nome = $("#nome")?.value;
@@ -12,6 +55,9 @@
       return;
     }
 
+    patio().adicionar({ nome, placa, entrada: new Date() });
+  
   });
+
 
 })();
